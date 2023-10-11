7d82a0b9eca6e35f935437b675c4a8e7522a3eb2
Alexej Schwarz <alex.schwarz@softwareone.com>
2023-10-11 11:59:20 +0200

    trava invalidy track 2 name changed 2

--- src/app/shared/data-access/playlist-data/trava.ts
+++ src/app/shared/data-access/playlist-data/trava.ts
@@ -75,7 +75,7 @@ export const INVALIDY: Track[] = [
     url: `${environment.dataHostUrl}/audio/trava/2003-invalidy/trava-invalidy-01-solnyshko-krasnoe.mp3`
   },
   {
-    title: 'Не Пей Эту Воду',
+    title: 'Не Пей Эту Воду ',
     author: {
       music: 'Трава'
     }
