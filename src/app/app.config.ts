d96886b81fd1ec2c4818c82b9ed633e06eadb756
Alexej Schwarz <alex.schwarz@softwareone.com>
2024-06-11 12:25:58 +0200

    WIP 1

---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import {
+  PreloadAllModules,
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+  withPreloading
+} from '@angular/router'
+
+import { routes } from './app.routes'
+import { provideClientHydration } from '@angular/platform-browser'
+import { provideHttpClient } from '@angular/common/http'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled'
+      }),
+      withPreloading(PreloadAllModules),
+      withEnabledBlockingInitialNavigation()
+    ),
+    provideClientHydration(),
+    provideHttpClient()
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
 import { AppComponent } from './app/app.component'
-import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser'
-import { routes } from './app/app.routes'
-import {
-  PreloadAllModules,
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-  withInMemoryScrolling,
-  withPreloading
-} from '@angular/router'
-import { provideHttpClient } from '@angular/common/http'
+import { bootstrapApplication } from '@angular/platform-browser'
+import { appConfig } from './app/app.config'
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        anchorScrolling: 'enabled'
-      }),
-      withPreloading(PreloadAllModules),
-      withEnabledBlockingInitialNavigation()
-    ),
-    provideClientHydration(),
-    provideHttpClient()
-  ]
-}).catch(err => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err))
